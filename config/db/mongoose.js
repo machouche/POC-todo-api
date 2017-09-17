92112b2396e0c8e8d9a7fe8307a4c951e3ef5b6b
machouche <maxime.achouche@gmail.com>
2017-09-17 20:36:38 +0200

    adding delete and update methods, adding mongoose config

--- config/db/mongoose.js
+++ config/db/mongoose.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 const { dbUrl } = require('./../properties');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(dbUrl);
+
+mongoose.connect(dbUrl, {
+    useMongoClient: true
+}).then((connection) => {
+    connection.openUri(dbUrl);
+}).catch(e => console.log(e));
+
 
 module.exports = { mongoose };
--- routes/todos.js
+++ routes/todos.js
@@ -1,23 +1,72 @@
+const { ObjectID } = require('mongodb');
 const express = require('express');
 const router = express.Router();
 const mongoose = require('../config/db/mongoose');
 const Todo = require('../config/models/todo');
 
-
 router.get('/', (req, res) => {
     const todo = new Todo();
-    Todo.find().then(doc => res.send(doc));
+
+    Todo.find().then(
+        todos => res.send(todos),
+        err => res.status(404).send(err));
+});
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (!ObjectID.isValid) return res.status(404).send();
+
+
+    Todo.findById(id).then(todo => {
+        if (!todo) return res.status(404).send();
+
+        res.send(todo);
+    }).catch(e => res.status(400).send());
 });
 
 router.post('/', (req, res) => {
     const todo = new Todo({ text: req.body.text });
-    res.send();
-    todo.save().then(() => res.status(400).send(),
+
+    todo.save().then(
+        todo => res.send(todo),
         err => res.status(400).send(err)
     );
 });
 
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
 
+    if (!ObjectID.isValid) return res.status(404).send();
+
+    Todo.findByIdAndRemove(id)
+        .then((todo) => {
+
+            if (!todo) return res.status(404).send();
+
+            res.send(todo);
+        }).catch(e => res.status(400).send())
+});
+
+
+router.patch('/:id', (req, res) => {
+    const id = req.params.id;
+    const { completed } = req.body;
+    const body = {};
+    if (!ObjectID.isValid(id)) return res.status(404).send();
+
+    const $set = {
+        completedAt: completed === true ? new Date().getTime() : null,
+        completed
+    }
+
+    Todo.findByIdAndUpdate(id, { $set }, { new: true })
+        .then(todo => {
+            if (!todo) return res.status(404).send();
+            res.send(todo);
+        })
+        .catch(e => res.status(400).send());
+});
 
 
 
